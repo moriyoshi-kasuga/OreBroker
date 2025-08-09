1c77d86103d65ab7d16305de23c6ef75246b7ddc
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-09 17:51:03 +0900

    feat: add lobby world

--- src/main/java/github/moriyoshi/orebroker/OreBroker.java
+++ src/main/java/github/moriyoshi/orebroker/OreBroker.java
@@ -1,13 +1,26 @@
 package github.moriyoshi.orebroker;
 
+import org.bukkit.World.Environment;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OreBroker extends JavaPlugin {
     @Override
     public void onEnable() {
+        initWorld();
     }
 
     @Override
     public void onDisable() {
+
+    }
+
+    private void initWorld() {
+        new WorldCreator("lobby")
+                .environment(Environment.NORMAL)
+                .type(WorldType.FLAT)
+                .generateStructures(false)
+                .createWorld();
     }
 }
---
+++ src/main/java/github/moriyoshi/orebroker/constant/OreBrokerWorld.java
@@ -0,0 +1,16 @@
+package github.moriyoshi.orebroker.constant;
+
+import java.util.Objects;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+
+public final class OreBrokerWorld {
+    @NotNull
+    public static final World LOBBY = Objects.requireNonNull(Bukkit.getWorld("lobby"));
+
+    @NotNull
+    public static final Location LOBBY_LOC = new Location(LOBBY, -0.5, 1.0, -0.5);
+}
